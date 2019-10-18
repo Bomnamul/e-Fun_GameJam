f00f3ea45f7669377e91236085f2d60b4e1b3b81
yang <wdv1235@gmail.com>
2019-10-19 01:04:49 +0900

    Add Singleton

---
+++ Twenty_Four/Assets/Scripts/SceneMgr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMgr : MonoBehaviour
+{
+    public static SceneMgr instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    void Start()
+    {
+        SceneManager.LoadScene("In_Game", LoadSceneMode.Single);
+    }
+
+    void Update()
+    {
+        
+    }
+}
