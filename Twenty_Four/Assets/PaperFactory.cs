8e7904380c4de6b1eab6ceba5a06a9e3a0df8624
yang <wdv1235@gmail.com>
2019-10-19 05:08:14 +0900

    Add Minigame

---
+++ Twenty_Four/Assets/PaperFactory.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperFactory : MonoBehaviour
+{
+    public GameObject redPaper;
+    public GameObject greenPaper;
+    public GameObject bluePaper;
+    public int paperCount;
+
+    List<GameObject> redPoolList;
+    List<GameObject> greenPoolList;
+    List<GameObject> bluePoolList;
+    List<GameObject> unsortedList;
+
+    void Awake()
+    {
+        redPoolList = new List<GameObject>();
+        greenPoolList = new List<GameObject>();
+        bluePoolList = new List<GameObject>();
+        unsortedList = new List<GameObject>();
+    }
+
+    private void Start()
+    {
+        while (unsortedList.Count != paperCount)
+        {
+            int rnd = Random.Range(0, 3);
+            switch (rnd)
+            {
+                case 0:
+                    unsortedList.Add(Instantiate(redPaper, transform));
+                    break;
+                case 1:
+                    unsortedList.Add(Instantiate(greenPaper, transform));
+                    break;
+                case 2:
+                    unsortedList.Add(Instantiate(bluePaper, transform));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Twenty_Four/Assets/Scripts/PaperPool.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperPool : MonoBehaviour
+{
+    public GameObject redPaper;
+    public GameObject greenPaper;
+    public GameObject bluePaper;
+    public int poolCount;
+
+    List<GameObject> redPoolList;
+    List<GameObject> greenPoolList;
+    List<GameObject> bluePoolList;
+    List<GameObject> unsortedList;
+
+    private void Awake()
+    {
+        redPoolList = new List<GameObject>();
+        greenPoolList = new List<GameObject>();
+        bluePoolList = new List<GameObject>();
+    }
+
+    private void Start() // Instantiate 하기 전에 반드시 Active Scene을 바꿔줘야 함
+    {
+        for (int i = 0; i < poolCount; i++)
+        {
+            var rtemp = Instantiate(redPaper, transform);
+            rtemp.SetActive(false);
+            redPoolList.Add(rtemp);
+            var gtemp = Instantiate(greenPaper, transform);
+            gtemp.SetActive(false);
+            greenPoolList.Add(gtemp);
+            var btemp = Instantiate(bluePaper, transform);
+            btemp.SetActive(false);
+            bluePoolList.Add(btemp);
+        }
+    }
+}
